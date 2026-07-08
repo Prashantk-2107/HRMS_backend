a0e34101dbce4faa5fb7197b8b9687504bc16ed0
Prashant <prashantkamboj.work@gmail.com>
2026-07-08 11:12:41 +0530

    Changed nodemailer config and set ipv4

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,9 +1,22 @@
 import nodemailer from "nodemailer";
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.GMAIL_ID,
+//     pass: process.env.APP_PASSWORD,
+//   },
+// });
+
+// export { transporter };
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_ID,
     pass: process.env.APP_PASSWORD,
--- src/modules/mail/sendMail.js
+++ src/modules/mail/sendMail.js
@@ -2,6 +2,9 @@ import { transporter } from "../../config/nodemailer.js";
 
 const sendMail = async ({ to, subject, text }) => {
   try {
+    await transporter.verify();
+    console.log("SMTP Connected");
+
     await transporter.sendMail({
       from: process.env.GMAIL_ID,
       to,
